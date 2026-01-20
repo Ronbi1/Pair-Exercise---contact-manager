d6dc6caacb632b7f2918102ef05dec18420e6320
Ronbi1 <bitonr7@gmail.com>
2026-01-20 14:58:00 +0200

    first pair project- empty file - just structure

---
+++ app.js
@@ -0,0 +1,10 @@
+// console.log(process.argv);
+// const num1 = Number(process.argv[2]);
+// const op = process.argv[3];
+// const num2 = Number(process.argv[4]);
+// if (!isNaN(num1) && !isNaN(num2)) {
+//     let calc = calculator(num1, op, num2);
+//     console.log(calc);
+// } else {
+//     console.log("not a number");
+// }
