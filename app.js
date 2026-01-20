fa3206d7c7f8d88860d62d51baf95c7c41fe59fa
Ronbi1 <bitonr7@gmail.com>
2026-01-20 22:01:06 +0200

    Refactor command handling logic into dedicated commandHandler module

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const contactService = require('./services/contactService');
+const commandHandler = require('./commands/commandHandler');
 
 console.log(process.argv);
 const command = process.argv[2];
@@ -7,16 +7,7 @@ console.log("command", command);
 const arg1 = process.argv[3];
 const arg2 = process.argv[4];
 const arg3 = process.argv[5];
-switch (command) {
-    case 'add': contactService.addContact(arg1, arg2, arg3);
-        break;
-    case 'list': contactService.listContacts();
-        break;
-    case 'delete': contactService.deleteContacts(arg1);
-        break;
-    case 'search': contactService.searchContacts(arg1);
-        break;
-    case 'help': contactService.help();
-        break;
-    default: console.log('Unknown command', command);
-}
+commandHandler.handleCommand(command, arg1, arg2, arg3);
+
+
+
